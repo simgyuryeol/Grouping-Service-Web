8d3e4752f19e998f0dbbc67d9641c99d7a86dbe7
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-04-29 03:00:14 +0900

    프로젝트 생성

---
+++ src/main/java/grouping/grouping_project/GroupingProjectApplication.java
@@ -0,0 +1,13 @@
+package grouping.grouping_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grouping/grouping_project/GroupingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package grouping.grouping_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
